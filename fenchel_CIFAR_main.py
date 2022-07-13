restructure, fix the sign of inf term

fenchel_CIFAR_main.py
@@ -11,11 +11,12 @@ from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 
 from torchvision import models
-from src.dataset import return_data
-from src.fenchel_classifier import ImageClassifier
-from src.model import Net, Net_influence
-from src.magic_module import MagicModule
-from src.utils import save_json
+from data.dataset import return_data
+from solver.fenchel_solver import FenchelSolver
+from modeling.classification_models import Net
+from modeling.influence_models import Net_influence
+from src.solver.magic_module import MagicModule
+from utils.utils import save_json
 
 EPSILON = 1e-5
 
@@ -26,7 +27,7 @@ def main(args):
     num_class = 10
     model = models.resnet34(pretrained=True).to('cuda')
     influence_model = Net_influence(1, num_class).to('cuda')
-    fenchen_classifier = ImageClassifier(x_test, y_test, model, influence_model)
+    fenchen_classifier = FenchelSolver(x_test, y_test, model, influence_model)
 
     fenchen_classifier.load_data("train", train_loader.dataset, args.batch_size, shuffle=True)
     fenchen_classifier.load_data("test", test_loader.dataset, args.batch_size, shuffle=False)
@@ -41,7 +42,7 @@ def main(args):
 
     for epoch in range(args.max_epoch):
         fenchen_classifier.train_epoch()
-        fenchen_classifier.save_checkpoint(args.ckpt_dir + args.ckpt_name)
+        fenchen_classifier.save_checkpoint(args._ckpt_dir + args.ckpt_name)
         result = {}
         
         train_dataset_size = len(train_loader.dataset)

fenchel_GMM1D_main.py
@@ -1,94 +1,93 @@
 import argparse
 import os
 import numpy as np
-from sklearn.utils import shuffle
 from tqdm import tqdm
-import copy
 
 import torch
-from torch import nn
-from torch import optim
-from torch.utils.data import TensorDataset, DataLoader
 
-from torchvision import models
-from src.utils import save_json
-from src.dataset import return_data
-from src.fenchel_classifier import ImageClassifier
+from src.utils.utils import save_json
+from src.utils.dataset import return_data
+from src.solver.fenchel_solver import FenchelSolver
 
-from src.magic_module import MagicModule
-from src.model import GMM1D_influence, LogisticRegression
+from src.modeling.influence_models import GMM1D_IF
+from src.modeling.classification_models import LogisticRegression
+
+import wandb
+import yaml
+from types import SimpleNamespace
+
+os.chdir('/home/xiaochen/kewen/IF_project')
 EPSILON = 1e-5
 
+
+YAMLPath = 'src/config/GMM1D/GMM1D_exp09.yaml'
+
 def main(args):
-    train_loader, test_loader = return_data(args.batch_size, "GMM1D")
+    train_dataset, test_dataset = return_data(args.batch_size, "GMM1D")
     input_dim = 1
     num_class = 2
-    x_test, y_test = test_loader.dataset[args.test_id_num]
-    x_test, y_test = test_loader.collate_fn([x_test]).float(), test_loader.collate_fn([y_test]).float()
+    x_test, y_test = test_dataset[args.test_id_num]
+    x_test, y_test = torch.FloatTensor([[x_test]]), torch.IntTensor([y_test])
 
-    model = LogisticRegression(input_dim,num_class).to('cuda') # 2class
-    # influence_models = nn.ModuleList([GMM1D_influence(input_dim,1).to('cuda')]*num_class) # influence score
-    influence_model = GMM1D_influence(input_dim, 1, num_class).to('cuda') # influence score
-    fenchen_classifier = ImageClassifier(x_test, y_test, model = model, influence_model = influence_model)
+    if args.classification_model == 'LogisticRegression':
+        classification_model = LogisticRegression(input_dim,num_class).to('cuda') # 2class
+    else:
+        raise NotImplementedError()
+    
+    if args.influence_model == 'GMM1D_IF':
+        influence_model = GMM1D_IF(input_dim, 1, num_class).to('cuda') # influence score
+    else:
+        raise NotImplementedError()
 
-    fenchen_classifier.load_data("train", train_loader.dataset, args.batch_size, shuffle=True)
-    fenchen_classifier.load_data("test", test_loader.dataset, args.batch_size, shuffle=False)
+    fenchen_classifier = FenchelSolver(x_test, y_test, classification_model = classification_model, influence_model = influence_model, softmax_temp=args.softmax_temp)
+
+    fenchen_classifier.load_data("train", train_dataset, args.batch_size, shuffle=True)
+    fenchen_classifier.load_data("test", test_dataset, args.batch_size, shuffle=False)
     
 
     fenchen_classifier.init_weights(
-        n_examples=len(train_loader.dataset),
-        w_init=args.w_init,
-        w_decay=args.w_decay)
+        n_examples=len(train_dataset),
+        w_init=args.influence_weight_init,
+        w_decay=args.influence_weight_decay)
 
-    fenchen_classifier.get_optimizer(args.image_lr, args.image_momentum, args.image_weight_decay)
+    fenchen_classifier.get_optimizer(args.classification_lr, args.classification_momentum, args.classification_weight_decay)
 
     for epoch in range(args.max_epoch):
+        
         fenchen_classifier.train_epoch()
-        fenchen_classifier.save_checkpoint(args.ckpt_dir + args.ckpt_name)
+        fenchen_classifier.save_checkpoint(args._ckpt_dir + args.ckpt_name)
         
         result = {}
-        train_dataset_size = len(train_loader.dataset)
+        train_dataset_size = len(train_dataset)
         influences = [0.0 for _ in range(train_dataset_size)]
         # TODO compute by batch
         for i in tqdm(range(train_dataset_size)):
-            x, y = train_loader.dataset[i]
+            x, y = train_dataset[i]
             influences[i] = influence_model(torch.cuda.FloatTensor([x.item()]).unsqueeze(0),torch.cuda.LongTensor([y.item()])).cpu().item()
         influences = np.array(influences)
-        harmful = np.argsort(influences)
-        helpful = harmful[::-1]
+        helpful = np.argsort(influences)
+        harmful = helpful[::-1]
         result["helpful"] = helpful[:500].tolist()
         result["harmful"] = harmful[:500].tolist()
         result["influence"] = influences.tolist()
-        json_path = os.path.join(args.output_path, f"IF_GMM1D_testid_{args.test_id_num}_epoch_{epoch}.json")
+        json_path = os.path.join("outputs", args.dataset_name, f"IF_GMM1D_testid_{args.test_id_num}_epoch_{epoch}.json")
         save_json(result, json_path)
+        helpful_data = [ [train_dataset[x][0].item(), train_dataset[x][1].item()] for x in helpful[:100]]
+        table = wandb.Table(data=helpful_data, columns = ["x", "class"])
+        wandb.log({f"helpful_data_epoch{epoch}" : wandb.plot.scatter(table,
+                                    "x", "class")})
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    with open(YAMLPath) as file:
+        config = yaml.safe_load(file)   
+        wandb.init(
+            project="IF_PROJECT",
+            name = f"{config['dataset_name']}_test_id_{config['test_id_num']}",
+            config=config
+        )
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(config["_gpu_id"])
+        config = SimpleNamespace(**config)
 
-    parser.add_argument('--seed', default=1, type=int, help='random seed')
-    parser.add_argument('--gpu', default=1, type=int, help='gpu index')
-    parser.add_argument('--max_epoch', default=10, type=float, help='maximum training epoch')
-    parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('--ckpt_dir', default='../checkpoints/fenchel/', type=str, help='checkpoint directory')
-    parser.add_argument('--ckpt_name', default='last', type=str, help='load previous checkpoint. insert checkpoint filename')
-    parser.add_argument('--num_workers', default=2, type=int, help='dataloader num_workers')
-    parser.add_argument('--display_step', default=2000, type=int, help='number of iterations after which loss data is printed and visdom is updated')
-    parser.add_argument('--batch_size', default=64, type=int, help='batch size')
-
-    parser.add_argument('--image_lr', default=1e-3, type=float)
-    parser.add_argument('--image_momentum', default=0.9, type=float)
-    parser.add_argument('--image_weight_decay', default=0.01, type=float)
-
-    # weighting
-    parser.add_argument("--w_decay", default=10., type=float)
-    parser.add_argument("--w_init", default=0., type=float)
-    parser.add_argument('--output_path', type=str, default="../outputs/GMM1D", help="where to put images")
-
-    parser.add_argument('--test_id_num', type=int, default=1, help="id of test example in testloader, 0 is [-1,0], 1 is [1,1]")
-    args = parser.parse_args()
-
-    # Target
-
-    main(args)
+    main(config)

hvp_tracIn_main.py
@@ -2,12 +2,12 @@ import os
 import torch
 import json
 import argparse
-from kewen.IF_project.fenchel import fenchel_calc
+# from kewen.IF_project.fenchel import fenchel_calc
 
-from src.utils import save_json
-from src.dataset import return_data
-from src.tracIn import tracin_cp
-from src.hvp_grad import calc_influence_single
+from utils.utils import save_json
+from data.dataset import return_data
+from src.baseline.tracIn import tracin_cp
+from src.baseline.hvp_grad import calc_influence_single
 
 def main(args):
     device = torch.device("cuda:{}".format(args.gpu))

src/solver/fenchel_solver.py
@@ -11,18 +11,17 @@ from torch.utils.data import TensorDataset, DataLoader
 from torchvision import models
 
 from .magic_module import MagicModule
+import wandb
 
-from .model import Net
 EPSILON = 1e-5
 
 
-class ImageClassifier:
-    def __init__(self, x_test, y_test, model, influence_model, pretrained=True,
+class FenchelSolver:
+    def __init__(self, x_test, y_test, classification_model, influence_model, pretrained=True,
                  baseline=False, softmax_temp=1.):
                  
         self._influence_model = influence_model
-        # self._model = models.resnet34(pretrained=pretrained).to('cuda')
-        self._model = model
+        self._classification_model = classification_model
 
         self._optimizer_theta3 = None
 
@@ -60,7 +59,7 @@ class ImageClassifier:
     
     def get_optimizer(self, learning_rate, momentum, weight_decay):
         self._optimizer_theta3 = optim.SGD(
-            self._model.parameters(),
+            self._classification_model.parameters(),
             lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
         self._optimizer_theta1 = optim.SGD(
             self._influence_model.parameters(),
@@ -81,11 +80,13 @@ class ImageClassifier:
             weights= softmax_normalize(
                 self._weights[ids].detach(),
                 temperature=self._softmax_temp)
+
+            weights_varience = torch.var(weights)
+            wandb.log({'weights_varience': weights_varience})
             self._optimizer_theta1.zero_grad()
-            # concatenate inputs and labels
-            # inputs_labels = torch.cat([inputs, labels.unsqueeze(1)], dim=1)
-            loss_F = - torch.sum(torch.mean( self._influence_model(inputs, labels) - self._influence_model(inputs, labels).flatten() * weights))
-            loss_F = torch.log(loss_F+10) 
+
+            loss_F = torch.sum(self._influence_model(inputs, labels).flatten() * weights) - torch.mean( self._influence_model(inputs, labels).flatten() )
+            # loss_F = torch.log(loss_F+10) 
             loss_F.backward() #theta 1 update, todo: does weights change?
             self._optimizer_theta1.step()
 
@@ -100,7 +101,7 @@ class ImageClassifier:
             self._optimizer_theta3.zero_grad()
             lossHistory = []
             while 1:
-                logits = self._model(inputs)
+                logits = self._classification_model(inputs)
                 loss = criterion(logits, labels)
                 loss = torch.sum(loss * weights.data)
                 loss.backward() #theta 3 update
@@ -112,19 +113,15 @@ class ImageClassifier:
                 if len(lossHistory) > 5:
                     if torch.var(torch.stack(lossHistory[-5:])).item() < 0.1:
                         break
-            # logits = self._model(inputs)
-            # loss = criterion(logits, labels)
-            # loss = torch.sum(loss * weights.data)
-            # loss.backward() #theta 3 update
-            # self._optimizer_theta3.step()
                     
-
+            wandb.log({'uniform_minus_weighted_influence': loss_F, 'classification_loss': loss})
             if self.global_iter % 200 == 0:
                 pbar.write('[{}] loss_F: {:.3f}, loss: {:.3F} '.format(
                             self.global_iter, loss_F, loss))
+                
 
     def _get_weights(self, batch, no_update = False): # batch is from train set
-        self._model.eval()
+        self._classification_model.eval()
 
         inputs, labels, ids = tuple(t.to('cuda') for t in batch)
         batch_size = inputs.shape[0]
@@ -132,10 +129,10 @@ class ImageClassifier:
         weights = self._weights[ids]
         if no_update:
             return weights
-        magic_model = MagicModule(self._model)
+        magic_model = MagicModule(self._classification_model)
         criterion = nn.CrossEntropyLoss()
 
-        model_tmp = copy.deepcopy(self._model)
+        model_tmp = copy.deepcopy(self._classification_model)
         optimizer_hparams = self._optimizer_theta3.state_dict()['param_groups'][0]
         optimizer_tmp = optim.SGD(
             model_tmp.parameters(),
@@ -144,7 +141,7 @@ class ImageClassifier:
             weight_decay=optimizer_hparams['weight_decay'])
 
         for i in range(batch_size):
-            model_tmp.load_state_dict(self._model.state_dict())
+            model_tmp.load_state_dict(self._classification_model.state_dict())
             optimizer_tmp.load_state_dict(self._optimizer_theta3.state_dict())
 
             model_tmp.zero_grad()
@@ -161,7 +158,7 @@ class ImageClassifier:
 
             deltas = {}
             for (name, param), (name_tmp, param_tmp) in zip(
-                    self._model.named_parameters(),
+                    self._classification_model.named_parameters(),
                     model_tmp.named_parameters()):
                 assert name == name_tmp
                 deltas[name] = weights[i] * (param_tmp.data - param.data)
@@ -179,25 +176,17 @@ class ImageClassifier:
         magic_model.eval() # batchnorm will give error if we don't do this
         test_logits = magic_model(self.x_test) #this part is magic_module
         test_loss = criterion(test_logits, self.y_test.long()) #the third term
-        # inputs_labels = torch.cat([train_inputs, train_labels.unsqueeze(1)], dim=1)
+
         weights = softmax_normalize(
                     weights,
                     temperature=self._softmax_temp)
-        infTerm = test_loss - torch.sum(self._influence_model(inputs,labels).squeeze().detach() * weights)  # the inf term, sum? todo: check this
-        print("infTerm", infTerm)
-        # test_loss = test_loss * float(batch_size) / float(
-        #     len(self._dataset['dev'])) #not using dev but train batch
+        infTerm = test_loss - torch.sum(self._influence_model(inputs,labels).squeeze().detach() * weights)
 
         weights_grad = torch.autograd.grad(
             infTerm, weights, retain_graph=True)[0]
         weights_grad_list.append(weights_grad)
-#       weight_grad += self._influence_model(train_inputs)
+
         weights_grad = sum(weights_grad_list)
-
-        print("weights_grad average before clip", sum(weights_grad) / len(weights_grad))
-        # weights_grad = torch.min(weights_grad, torch.ones_like(weights_grad))
-        # weights_grad = torch.max(weights_grad, -torch.ones_like(weights_grad))
-
 
         self._weights[ids] = weights.data / self._w_decay - weights_grad
         self._weights[ids] = torch.max(self._weights[ids], torch.ones_like(
@@ -207,8 +196,9 @@ class ImageClassifier:
             self._weights[ids] = torch.zeros_like(self._weights[ids])
         return self._weights[ids].data
 
+    # TODO Get rid of evaulate and test dataloader
     def evaluate(self, set_type):
-        self._model.eval()
+        self._classification_model.eval()
 
         preds_all, labels_all = [], []
         for batch in tqdm(self._data_loader[set_type],
@@ -217,7 +207,7 @@ class ImageClassifier:
             inputs, labels, _ = batch
 
             with torch.no_grad():
-                logits = self._model(inputs)
+                logits = self._classification_model(inputs)
 
             preds = torch.argmax(logits, dim=1)
             preds_all.append(preds)

src/utils/dataset.py
@@ -20,28 +20,28 @@ def return_data(batch_size, dataset_name):
 
         train_data = torchvision.datasets.CIFAR10(root='../data', train=True,
                                                 download=True, transform=transform)
-        train_loader = DataLoader(train_data, batch_size=batch_size,
-                                shuffle=True, num_workers=num_workers)
+        # train_loader = DataLoader(train_data, batch_size=batch_size,
+        #                         shuffle=True, num_workers=num_workers)
 
         test_data = torchvision.datasets.CIFAR10(root='../data', train=False,
                                                 download=True, transform=transform)
-        test_loader = DataLoader(test_data, batch_size=batch_size,
-                                shuffle=True, num_workers=num_workers)
+        # test_loader = DataLoader(test_data, batch_size=batch_size,
+        #                         shuffle=True, num_workers=num_workers)
 
         print('loaded train_dataset with {} samples,test_dataset with {} samples, '.format(len(train_data),len(test_data) ))
         
-        return train_loader, test_loader
+        return train_data, test_data
     elif dataset_name == "GMM1D":
-        train_df = pd.read_csv("../data/1D_1000sample_2class_train.csv")
+        train_df = pd.read_csv("data/1D_1000sample_2class_train.csv")
         train_data = TensorDataset(torch.tensor(train_df['x']).reshape(-1,1), torch.tensor(train_df['y']).reshape(-1,1))
-        train_loader = DataLoader(train_data, batch_size=batch_size,
-                                shuffle=True, num_workers=1)
+        # train_loader = DataLoader(train_data, batch_size=batch_size,
+        #                         shuffle=True, num_workers=1)
 
-        test_df = pd.read_csv("../data/1D_1000sample_2class_test.csv")
+        test_df = pd.read_csv("data/1D_1000sample_2class_test.csv")
         test_data = TensorDataset(torch.tensor(test_df['x']).reshape(-1,1), torch.tensor(test_df['y']))
-        test_loader = DataLoader(test_data, batch_size=batch_size,
-                                shuffle=True, num_workers=1)
+        # test_loader = DataLoader(test_data, batch_size=batch_size,
+        #                         shuffle=True, num_workers=1)
 
         print('loaded train_dataset with {} samples,test_dataset with {} samples, '.format(len(train_data),len(test_data) ))
 
-        return train_loader, test_loader
+        return train_data, test_data